64da59e9930cd6ebb1216a2ad60b1e6081282722
Eid <Eid@192.168.0.8>
2018-11-07 22:33:28 -0500

    test case for family A passed for hours before 11 PM only.

--- Babysitter Kata/src/WageCalculator.java
+++ Babysitter Kata/src/WageCalculator.java
@@ -11,8 +11,40 @@ public class WageCalculator {
 	}
 
 
+	public String getStartTime() {
+		return startTime;
+	}
+
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+
 	
-	
+	public int familyAWageCalculation(String startTime, String endTime) {
+		String parsingStartTime = startTime.substring(0,2) + startTime.substring(3,5); //parses the string to retrieve the time value excluding :
+		String parsingEndTime =  endTime.substring(0,2) + endTime.substring(3,5);     // input "16:53" becomes "1653" for later calculations
+		
+		int parseStartTimeInteger = Integer.parseInt(parsingStartTime);
+		int parseEndTimeInteger = Integer.parseInt(parsingEndTime);
+		int timeWorked = parseEndTimeInteger - parseStartTimeInteger;
+		double hoursWorked = Math.ceil(timeWorked/100.0);
+		System.out.println(timeWorked/100);
+		int totalPay = (int)hoursWorked * 15;
+
+		return totalPay;
+	}
 	
 	
 
--- Babysitter Kata/test/WageCalculatorTest.java
+++ Babysitter Kata/test/WageCalculatorTest.java
@@ -1,22 +1,59 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class WageCalculatorTest {
 
 	WageCalculator wageCalculator;
-
+	private String startTime;
+	private String endTime;
 	@Before
 	public void setup()  {
-		
-	
-
-	
-		
-	
-
-		
+		wageCalculator = new WageCalculator(startTime,endTime);	
 		
 	}
+	//Since only proper inputs will get to this point, I will only test proper inputs since that is the only
+	// possible scenario that will occur, due to the error catching of the previous methods.
+	
+	
+	@Test
+	public void familyABeforeEleven11PMOnlyWageCalculation () {
+		wageCalculator = new WageCalculator(startTime,endTime);
+		wageCalculator.setStartTime("17:00");  //From 5Pm to 10:59 PM
+		wageCalculator.setEndTime("22:59");
+		
+		assertEquals(90,wageCalculator.familyAWageCalculation(wageCalculator.getStartTime(), wageCalculator.getEndTime()));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Tear down method after all the tests are completed.
+	@After
+	public void tearDown() throws Exception {
+	    wageCalculator = null;
+	    assertNull(wageCalculator);
+	}
+	
 }
